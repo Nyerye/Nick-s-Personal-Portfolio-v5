59de37a59c317b2f2870e97af4065711fedfbb93
Nicholas Reilly <nick_reilly@outlook.com>
2025-04-15 12:19:43 -0400

    Backend Update
    Added the Download Resume functionality
    
    Created resume controllers and uploaded the resume I want employers to be able to download.

--- NickPortv5/backend/src/app.module.ts
+++ NickPortv5/backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContactModule } from './contact/contact.module';
+import { ResumeController } from './resume/resume.controller';
 
 @Module({
   imports: [ContactModule],
+  controllers: [ResumeController],
 })
 export class AppModule {}
