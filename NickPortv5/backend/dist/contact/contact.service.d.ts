685a02b3ca8dfd206014e983f0a36038e1fc1c21
Nicholas Reilly <nick_reilly@outlook.com>
2025-05-07 12:57:25 -0400

    Move site files to root for GitHub Pages

--- NickPortv5/backend/dist/contact/contact.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class ContactService {
-    sendEmail(name: string, email: string, subject: string, message: string): Promise<void>;
-}
--- NickPortv5/backend/dist/resume/resume.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from 'express';
-export declare class ResumeController {
-    downloadResume(res: Response): void;
-}
